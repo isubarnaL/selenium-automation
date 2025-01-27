49ccd40e6d25c355cc8dee52553307a9a6953166
isubarna1 <lohanisubarna@gmail.com>
2025-01-27 13:20:55 +0545

    styles.

--- test.py
+++ test.py
@@ -6,15 +6,13 @@ edge_driver_path = os.path.join("venv", "edgedriver_win32", "msedgedriver.exe")
 service = Service(edge_driver_path)
 
 driver = webdriver.Edge(service=service)
-
 driver.get("https://www.linkedin.com")
-
 
 expected_title = "LinkedIn: Log In or Sign Up"
 actual_title = driver.title
 
 if actual_title == expected_title:
-    print("Title verification passed!")
+    print("Title verification passed.")
 else:
     print(f"Title verification failed! Expected: '{expected_title}', but got: '{actual_title}'")
 
